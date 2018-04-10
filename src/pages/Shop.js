5d925a64fd8c821957184abc3fe9984e3c661f02
Phong <phongnguyenhh1996@gmail.com>
2018-04-10 10:30:56 +0700

    update shop + product list

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, IndexRoute, Route, Link } from "react-router-dom";
 
 class Shop extends React.Component{
 	render(){
 		return(
 	<div className="container-fluid page-shop">
-			     <div className="row px-md-5 pb-md-5">
-	<div id="primary" className="col-xs-12 col-md-8 col-xl-9">
+	<div className="row px-md-5 py-md-3">
+					<nav aria-label="breadcrumb ">
+					  <ol class="breadcrumb bg-white">
+					    <li class="breadcrumb-item"><Link to="/">Home</Link></li>
+					    <li class="breadcrumb-item active" aria-current="page">Shop</li>
+					  </ol>
+					</nav>
+					<div className="w-100 border-bottom-dashed mx-md-3"></div>
+				</div>
+			     <div className="row px-md-5 py-md-4">
+	<div id="primary" className="col-xs-12 col-md-9 col-xl-9">
 <div className="row products">
 
 		<div id="products" className="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
@@ -48,138 +58,147 @@ class Shop extends React.Component{
 	</div>
 	</div>
 
-<div id="sidebar" className="col-xs-12 col-md-4 col-xl-3 sidebar widget-area" role="complementary">
-<aside id="aside-browse" className="browse"><h3 className="widget-title"><span>Browse</span>
-</h3><ul className="product-categories">
-<li className="nav-item1"><a  href="#">Amino Acids</a><span> (10)</span> </li>
-<li className="nav-item1"><a  href="#">Antioxidants</a> <span> (13)</span></li>
-<li className="nav-item1"><a  href="#">Multivitamins</a> <span> (15)</span></li>
-<li className="nav-item1"><a href="#">Organic Cosmetics</a> <span> (9)</span></li>
-<li className="nav-item1"><a href="#">Organic Food</a><span> (5)</span></li>
-<li className="nav-item1"><a  href="#">Vitamins &amp; Minerals</a><span> (19)</span> </li>
-</ul></aside>
-<aside id="aside-top-rated" className="top-rated"><h3 className="widget-title"><span>Top Rated Products</span>
-</h3>
-<ul className="product-list">
-<li>
-	<a href="#" title="Daily Detangling Shampoo">
-<div className="images1">
-	<img  width="75" height="75" src={require('../images/Daily Detangling Shampoo.png')} /></div>
-	<ul className="product-widget">
-	<li className="nav-item2"><a href="#">Amino Acids</a></li>
-<li className="nav-item2"><a href="#">Antioxidants</a></li>
-<li className="nav-item2"><a href="#">Multivitamins</a></li>
-<div className="product_title_link"><a href="#" title="Daily Detangling Shampoo">
-		<span className="product-title">Daily Detangling Shampoo</span>
-	</a>
-	</div>
-	<span className="price">15.00<span className="Price-currencySymbol">$
-	</span></span>–<span className="price">21.00<span className="Price-currencySymbol">$</span></span>
-	<p>
-<span className="fa fa-star checked"></span>
-<span className="fa fa-star checked"></span>
-<span className="fa fa-star checked"></span>
-<span className="fa fa-star"></span>
-<span className="fa fa-star"></span></p>
-</ul>
-</a>
-</li>
-<li>
-	<a href="#" title="Eminence Organic">
-	<div className="images2">
-	<img width="75" height="75" src={require('../images/Eminence Organic.png')} /></div>
-	<ul className="product-widget">
-	
-	<li className="nav-item2"><a href="#">Amino Acids</a></li>
-<li className="nav-item2"><a href="#">Antioxidants</a></li>
-	</ul>
-	<div className="product_title_link"><a href="#" title="Eminence Organic">
-		<span className="product-title">Eminence Organic</span>
-	</a>
-	</div>
-	<span className="price"><ins><span className="Price-amount">40.00
-	<span className="Price-currencySymbol">$</span></span></ins> <del><span className="woocommerce-Price-amount amount">
-	46.00<span className="Price-currencySymbol">$</span></span></del></span>
-<div className="fa-star">
-<span className="fa fa-star checked"></span>
-<span className="fa fa-star checked"></span>
-<span className="fa fa-star checked"></span>
-<span className="fa fa-star"></span>
-<span className="fa fa-star"></span></div>	</a>
-	</li>
+<div className="col-md-3">
+						<div className="row">
+							<div className="col-md-12 browse aside">
+								<h4 className="font-weight-bold w-100 pt-md-5">Browse</h4>
+								<ul className="nav flex-column pt-md-4">
+				                  <li className="nav-item">
+				                    <a className="nav-link px-md-0" href="#">Amino Acids <span>(19)</span></a>
+				                  </li>
+				                  <li className="nav-item">
+				                    <a className="nav-link px-md-0" href="#">Antioxidants <span>(19)</span></a>
+				                  </li>
+				                  <li className="nav-item">
+				                    <a className="nav-link px-md-0" href="#">Multivitamins <span>(19)</span></a>
+				                  </li>
+				                  <li className="nav-item">
+				                    <a className="nav-link px-md-0" href="#">Organic Cosmetics <span>(19)</span></a>
+				                  </li>
+				                  <li className="nav-item">
+				                    <a className="nav-link px-md-0" href="#">Organic Food <span>(19)</span></a>
+				                  </li>
+				                  <li className="nav-item">
+				                    <a className="nav-link px-md-0" href="#">Vitamins & Minerals <span>(19)</span></a>
+				                  </li>
+				                </ul>
+							</div>
+							<div className="col-md-12 aside">
+								<h4 className="font-weight-bold w-100 pt-md-5 mt-md-5">Top Rated Products</h4>
+								<ul className="product-list pt-md-4">
+									<li>
+										<a href="#" title="Daily Detangling Shampoo">
+									<div className="images1">
+										<img  width="75" height="75" src={require('../images/Daily Detangling Shampoo.png')} /></div>
+										<ul className="product-widget">
+										<li className="nav-item2"><a href="#">Amino Acids</a></li>
+									<li className="nav-item2"><a href="#">Antioxidants</a></li>
+									<li className="nav-item2"><a href="#">Multivitamins</a></li>
+									<div className="product_title_link"><a href="#" title="Daily Detangling Shampoo">
+											<span className="product-title">Daily Detangling Shampoo</span>
+										</a>
+										</div>
+										<span className="price color-main2 font-weight-bold">15.00$
+										</span> – <span className="price color-main2 font-weight-bold">21.00$</span>
+										<p>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span></p>
+									</ul>
+									</a>
+									</li>
+									<li>
+										<a href="#" title="Eminence Organic">
+										<div className="images2">
+										<img width="75" height="75" src={require('../images/Eminence Organic.png')} /></div>
+										<ul className="product-widget">
+										
+										<li className="nav-item2"><a href="#">Amino Acids</a></li>
+									<li className="nav-item2"><a href="#">Antioxidants</a></li>
+										</ul>
+										<div className="product_title_link"><a href="#" title="Eminence Organic">
+											<span className="product-title">Eminence Organic</span>
+										</a>
+										</div>
+										<span className="price color-main2 font-weight-bold">15.00$
+										</span> – <span className="price color-main2 font-weight-bold">21.00$</span>
+										<p>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span></p>	</a>
+										</li>
 
 
-<li>
-	<a href="#" title="BEST Vitamin C">
-	<div className="images1">
-	<img width="75" height="75" src={require('../images/BEST Vitamin C.png')} />
-	</div>
-	<ul className="product-widget">
-	<li className="nav-item2"><a href="#">Amino Acids</a></li>
-<li className="nav-item2"><a href="#">Antioxidants</a></li>
-<li className="nav-item2"><a href="#">Multivitamins</a></li>
-	</ul>
-	<div className="product_title_link"><a href="#" title="BEST Vitamin C">
-		<span className="product-title">BEST Vitamin C</span>
-	</a>
-	</div>
-	<span className="price"> <ins><span>39.00<span> 
-	$</span></span></ins> <del><span>56.00<span >$
-	</span></span></del></span>	
+									<li>
+										<a href="#" title="BEST Vitamin C">
+										<div className="images1">
+										<img width="75" height="75" src={require('../images/BEST Vitamin C.png')} />
+										</div>
+										<ul className="product-widget">
+										<li className="nav-item2"><a href="#">Amino Acids</a></li>
+									<li className="nav-item2"><a href="#">Antioxidants</a></li>
+									<li className="nav-item2"><a href="#">Multivitamins</a></li>
+										</ul>
+										<div className="product_title_link"><a href="#" title="BEST Vitamin C">
+											<span className="product-title">BEST Vitamin C</span>
+										</a>
+										</div>
+										<span className="price color-main2 font-weight-bold">15.00$
+										</span> – <span className="price color-main2 font-weight-bold">21.00$</span>
+										<p>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span></p>
 
-	<p>
+										</a>
+										
+									</li>
 
-<span className="fa fa-star checked"></span>
-<span className="fa fa-star checked"></span>
-<span className="fa fa-star checked"></span>
-<span className="fa fa-star"></span>
-<span className="fa fa-star"></span></p>
+									</ul>
+							</div>
+							<div className="col-md-12 aside">
+								<h4 className="font-weight-bold w-100 pt-md-5 mt-md-3">Recent Reviews</h4>
+								<ul className="product-list pt-md-4">
+									<li className="pb-md-5">
 
-	</a>
-	
-</li>
+									<div className="images3">
+										<img  width="75" height="75" src={require('../images/Daily Detangling Shampoo.png')} /></div>
+										<em class="reviewer">by <span className="by">admin</span></em>
+									<div className="product_title_link"><a href="#" title="Daily Detangling Shampoo">
+											<span className="product-title">Daily Detangling Shampoo</span></a></div>
+										
+									</li>
+									<li className="pb-md-5">
 
-</ul>
-</aside>
+									<div className="images3">
+										<img  width="75" height="75" src={require('../images/Eminence Organic.png')} /></div>
+										<em class="reviewer">by <span className="by">admin</span></em>
+									<div className="product_title_link"><a href="#" title="Eminence Organic">
+											<span className="product-title">Eminence Organic</span></a></div>
+										
+									</li>
+									<li className="pb-md-5">
 
-<aside id="aside-recent-reviews" className="recent-reviews"><h3 className="widget-title"><span>Recent Reviews</span>
-</h3>
-<ul className="product-list">
-<li>
-
-<div className="images3">
-	<img  width="75" height="75" src={require('../images/Daily Detangling Shampoo.png')} /></div>
-	<em class="reviewer">by <span className="by">admin</span></em>
-<div className="product_title_link"><a href="#" title="Daily Detangling Shampoo">
-		<span className="product-title">Daily Detangling Shampoo</span></a></div>
-	
-</li>
-<li>
-
-<div className="images3">
-	<img  width="75" height="75" src={require('../images/Eminence Organic.png')} /></div>
-	<em class="reviewer">by <span className="by">admin</span></em>
-<div className="product_title_link"><a href="#" title="Eminence Organic">
-		<span className="product-title">Eminence Organic</span></a></div>
-	
-</li>
-<li>
-
-<div className="images3">
-	<img  width="75" height="75" src={require('../images/BEST Vitamin C.png')} /></div>
-	<em class="reviewer">by <span className="by">admin</span></em>
-<div className="product_title_link"><a href="#" title="BEST Vitamin C">
-		<span className="product-title">BEST Vitamin C</span></a></div>
-	
-</li>
+									<div className="images3">
+										<img  width="75" height="75" src={require('../images/BEST Vitamin C.png')} /></div>
+										<em class="reviewer">by <span className="by">admin</span></em>
+									<div className="product_title_link"><a href="#" title="BEST Vitamin C">
+											<span className="product-title">BEST Vitamin C</span></a></div>
+										
+									</li>
 
 
-</ul>
+								</ul>
+							</div>
+						</div>
 
-
-</aside>
-
-</div>
+				</div>
 </div>
 </div>
 		);
--- src/pages/product-list.js
+++ src/pages/product-list.js
@@ -98,14 +98,124 @@ class ProductList extends React.Component{
 				                    <a className="nav-link px-md-0" href="#">Vitamins & Minerals <span>(19)</span></a>
 				                  </li>
 				                </ul>
-				                <div className="filter-price px-md-4 my-md-5 pb-md-5">
+				                <div className="filter-price px-md-4 mt-md-5 pb-md-5">
 				                	<h4 className="font-weight-bold pt-md-5">Filter by price</h4>
 				                	<div className="filter my-md-5"><div></div></div>
 				                	<button className="btn">FILTER</button><span className="text-secondary float-right pt-md-2">Price: 15$ — 87$</span>
 				                </div>
 							</div>
 							<div className="col-md-12 aside">
-								<h4 className="font-weight-bold w-100 pt-md-5">Top Rated Products</h4>
+								<h4 className="font-weight-bold w-100 pt-md-5 mt-md-5">Top Rated Products</h4>
+								<ul className="product-list pt-md-4">
+									<li>
+										<a href="#" title="Daily Detangling Shampoo">
+									<div className="images1">
+										<img  width="75" height="75" src={require('../images/Daily Detangling Shampoo.png')} /></div>
+										<ul className="product-widget">
+										<li className="nav-item2"><a href="#">Amino Acids</a></li>
+									<li className="nav-item2"><a href="#">Antioxidants</a></li>
+									<li className="nav-item2"><a href="#">Multivitamins</a></li>
+									<div className="product_title_link"><a href="#" title="Daily Detangling Shampoo">
+											<span className="product-title">Daily Detangling Shampoo</span>
+										</a>
+										</div>
+										<span className="price color-main2 font-weight-bold">15.00$
+										</span> – <span className="price color-main2 font-weight-bold">21.00$</span>
+										<p>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span></p>
+									</ul>
+									</a>
+									</li>
+									<li>
+										<a href="#" title="Eminence Organic">
+										<div className="images2">
+										<img width="75" height="75" src={require('../images/Eminence Organic.png')} /></div>
+										<ul className="product-widget">
+										
+										<li className="nav-item2"><a href="#">Amino Acids</a></li>
+									<li className="nav-item2"><a href="#">Antioxidants</a></li>
+										</ul>
+										<div className="product_title_link"><a href="#" title="Eminence Organic">
+											<span className="product-title">Eminence Organic</span>
+										</a>
+										</div>
+										<span className="price color-main2 font-weight-bold">15.00$
+										</span> – <span className="price color-main2 font-weight-bold">21.00$</span>
+										<p>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span></p>	</a>
+										</li>
+
+
+									<li>
+										<a href="#" title="BEST Vitamin C">
+										<div className="images1">
+										<img width="75" height="75" src={require('../images/BEST Vitamin C.png')} />
+										</div>
+										<ul className="product-widget">
+										<li className="nav-item2"><a href="#">Amino Acids</a></li>
+									<li className="nav-item2"><a href="#">Antioxidants</a></li>
+									<li className="nav-item2"><a href="#">Multivitamins</a></li>
+										</ul>
+										<div className="product_title_link"><a href="#" title="BEST Vitamin C">
+											<span className="product-title">BEST Vitamin C</span>
+										</a>
+										</div>
+										<span className="price color-main2 font-weight-bold">15.00$
+										</span> – <span className="price color-main2 font-weight-bold">21.00$</span>
+										<p>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star checked color-main2 fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span>
+									<span className="fa fa-star text-secondary fa-xs mr-1"></span></p>
+
+										</a>
+										
+									</li>
+
+									</ul>
+							</div>
+							<div className="col-md-12 aside">
+								<h4 className="font-weight-bold w-100 pt-md-5 mt-md-3">Recent Reviews</h4>
+								<ul className="product-list pt-md-4">
+									<li className="pb-md-5">
+
+									<div className="images3">
+										<img  width="75" height="75" src={require('../images/Daily Detangling Shampoo.png')} /></div>
+										<em class="reviewer">by <span className="by">admin</span></em>
+									<div className="product_title_link"><a href="#" title="Daily Detangling Shampoo">
+											<span className="product-title">Daily Detangling Shampoo</span></a></div>
+										
+									</li>
+									<li className="pb-md-5">
+
+									<div className="images3">
+										<img  width="75" height="75" src={require('../images/Eminence Organic.png')} /></div>
+										<em class="reviewer">by <span className="by">admin</span></em>
+									<div className="product_title_link"><a href="#" title="Eminence Organic">
+											<span className="product-title">Eminence Organic</span></a></div>
+										
+									</li>
+									<li className="pb-md-5">
+
+									<div className="images3">
+										<img  width="75" height="75" src={require('../images/BEST Vitamin C.png')} /></div>
+										<em class="reviewer">by <span className="by">admin</span></em>
+									<div className="product_title_link"><a href="#" title="BEST Vitamin C">
+											<span className="product-title">BEST Vitamin C</span></a></div>
+										
+									</li>
+
+
+								</ul>
 							</div>
 						</div>
 					</div>
